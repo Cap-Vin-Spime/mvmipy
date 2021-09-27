13be03c6dd4889d56bce751c64861b3048abecb2
Cap-Vin-Spime <68153949+Cap-Vin-Spime@users.noreply.github.com>
2021-09-27 19:56:06 +0530

    Update Computer Science Record.py

--- Computer Science Record.py
+++ Computer Science Record.py
@@ -207,3 +207,293 @@ for line in lines:
     for y in x:
         print(y+"#",end="")
     print("")
+
+    
+#11
+#TO REMOVE ALL THE LINES THAT CONTAIN THE CHARACTER 'A' IN A FILE & WRITE IT TO ANOTHER FILE
+file=open(r"C:\Users\Jammy\Documents\Abishek\Python\TEXT1.txt","r")
+lines=file.readlines()
+file.close()
+file=open(r"C:\Users\Jammy\Documents\Abishek\Python\TEXT1.txt","w")
+file=open(r"C:\Users\Jammy\Documents\Abishek\Python\MODTEXT1.txt","w")
+for line in lines:
+	if 'a' in line :
+		file1.write(line)
+	else:
+		file.write(line)
+print("Lines that contain a character are removed from TEXT1")
+print("Lines that contain a character are added in MODTEXT1")
+file.close()
+file1.close()
+
+
+#12
+#TO READ CHARACTERS FROM KEYBOARD ONE BY ONE ALL LOWER CASE LETTERS STORED INSIDE A FILE "LOWER",
+#ALL UPPERCASE LETTERS STORED INSIDE A FILE "UPPER",& ALL OTHER CHARACTERS STORED INSIDE "OTHERS"
+f1=open(r"C:\Users\Jammy\Documents\Abhishek\Python\LOWER.txt","w")
+f2=open(r"C:\Users\Jammy\Documents\Abhishek\Python\UPPER.txt","w")
+f3=open(r"C:\Users\Jammy\Documents\Abhishek\Python\OTHERS.txt","w")
+print('Input "~" to stop execution.')
+while True:
+	c=input("Enter a single character :")
+	if c=="~":
+		break
+	elif c.islower():
+		f1.write(c)
+	elif c.isupper():
+		f2.write(c)
+	else:
+		f3.write(c)
+f1.close()
+f2.close()
+f3.close()
+
+
+#13
+#CREATE A BINARY FILE WITH NAME & ROLL NUMBER. SEARCH FOR A GIVEN ROLL NUMBER AND DISPLAY NAME, IF NOT FOUND DISPLAY
+#APPROPRIATE MESSAGE
+import pickle
+sdata=[]
+slist=[]
+totals=int(input("Enter number of students :"))
+for i in range(totals):
+	sdata["Roll no."]=int(input("Enter roll no. :"))
+	sdata["Name"]=input("Enter name :")
+	slist.append(sdata)
+	sdata=()
+a=open(r"C:Users\Jammy\Documents\Abhishek\Python\TEXT2.dat","wb")
+pickle.dump(slist,a)
+a.close()
+
+x=open(r"C:Users\Jammy\Documents\Abhishek\Python\TEXT2.dat","rb")
+slist=pickle.load(x)
+b=int(input("Enter the roll number of student to be searched :"))
+y=False
+for TEXT2 in slist:
+	if (TEXT2["Roll no."]==b):
+		y=True
+		print(TEXT2["Name"],"Found in file")
+if (y==False):
+	print("Data of student not found.")
+	
+
+#14
+#TO CREATE A BINARY FILE WITH ROLL NUMBER, NAME & MARKS, INPUT A ROLL NUMBER & UPDATE THE MARKS.
+import pickle
+
+sdata={}
+totals=int(input("Enter number of students :"))
+a=open(r"C:\Users\Jammy\Documents\Abhishek\Python\TEXT3.dat","wb")
+for i in range(totals):
+	sdata["Roll no"]=int(input("Enter roll no. :"))
+	sdata["Name"]=input("Enter name :")
+	sdata["Marks"]=float(input("Enter marks :"))
+	pickle.dump(sdata,a)
+	sdata={}
+a.close()
+
+#UPDATING MARKS
+
+found=False
+rollno=int(input("Enter roll number :"))
+a=open(r"C:\Users\Jammy\Documents\Abhishek\Python\TEXT3.dat","rb+")
+try:
+	while True:
+		pos=a.tell()
+		sdata=pickle.load(a)
+		if (sdata["Roll no"]==rollno):
+			sdata["Marks"]=float(input("Enter new marks :"))
+			
+			a.seek(pos)
+			pickle.dump(sdata,a)
+			found=True
+except EOFError:
+	if found==False:
+		print("Roll number not found.")
+	else:
+		print("Students marks updated successfully.")
+	a.close()
+	
+	
+#15
+#Input Data
+import csv
+with open(r'C:\Users\Jammy\Documents\Abhishek\Python\employee.txt','a') as csvfile:
+	mywriter = csv.writer(csvfile,delimiter=',')
+	ans='y'
+	while ans.lower()=='y':
+		eno=int(input("Enter Employee ID :"))
+		name=input("Enter Employee Name :")
+		mobno=int(input("Enter Employee Mobile No :"))
+		mywriter.writerow([eno,name,mobno])
+		ans=input("Do You Want To Enter More Data? (y/n):")
+ans='y'
+
+#Search Empid And Display The Records
+
+with open(r'C:\Users\Jammy\Documents\Abhishek\Python\employee.txt','r') as csvfile:
+	myreader = csv.reader(csvfile,delimiter=',')
+	ans='y'
+	while ans.lower()=='y':
+		found=False
+		e = int(input("Enter Employee ID to search :"))
+		for row in myreader:
+			if len(row)!=0:
+				if int(row[0])==e:
+					print("Name :",row[1])
+					print("Mobile no. :",row[2])
+					found=True
+					break
+				if not found:
+					print("Employee ID not found.")
+		ans = input("Do You Want To Search More? (y/n):")
+ans='y'
+
+with open(r'C:\Users\Jammy\Documents\Abhishek\Python\employee.txt','a') as csvfile:
+	x=csv.writer(csvfile)
+	x.writerow([eno,name,mobno])
+	
+	
+#16
+#Write a programme to create Lpush() and Lpop() function to do push and pop operation
+#on a stack using a list e.g. take a student information and push and pop the details
+def isEmpty(S):
+	if len(S)==0:
+		return True
+	else:
+		return False
+def Push(S,item):
+	S.append(item)
+	top=len(S)-1
+def Pop(S):
+	if isEmpty(S):
+		return "UNDERFLOW"
+	else:
+		val=S.pop()
+	return val
+#main function begins here
+S=[]
+top=None
+while True:
+	print("***STACK DEMONSTRATION***")
+	print("1. PUSH")
+	print("2. POP")
+	print("0. EXIT")
+	ch=int(input("Enter your choice :"))
+	if ch==1:
+		val=int(input("Enter the item of Push :"))
+		Push(S,val)
+	elif ch==2:
+		val=Pop(S)
+		if val=="UNDERFLOW":
+			print("STACK IS EMPTY")
+		else:
+			print("Deleted Item was:", val)
+	elif ch==0:
+		print("THANK YOU")
+		break
+		
+		
+#17
+use studentdb;
+create table student
+(roll int not null,
+studentname varchar(30) not null,
+class char(5) not null,
+section char(1) not null,
+classstream char(20) not null);
+desc student;
+insert into student values
+(1,"Akhil", "XII", "A", "Science"),
+(2, "Satya", "XII", "C", "Science"),
+(3, "Antony", "XII", "D", "Commerce"),
+(4, "Vishal", "XII", "E", "Humanities"),
+(5, "Deepak", "XII", "A", "Science"),
+(6, "Brij", "XII", "B", "Science");
+select * from student;
+alter table student
+add(Substream char(20) not null),
+(Percentage float not null);
+desc student;
+update student set Substream = "Computer Science" where roll = 1;
+update student set Substream = "Biology" where roll = 2;
+update student set Substream = "Maths" where roll = 3;
+update student set Substream = "NA" where roll = 4;
+update student set Substream = "Computer Science" where roll = 5;
+update student set Substream = "Computer Science" where roll = 6;
+select * from student;
+update student set Percentage = 81 where roll = 1;
+update student set Percentage = 69 where roll = 2;
+update student set Percentage = 92 where roll = 3;
+update student set Percentage = 55 where roll = 4;
+update student set Percentage = 85 where roll = 5;
+update student set Percentage = 72 where roll = 6;
+alter table student add(Adhaar_card biginit not null);
+desc student;
+alter table student
+drop coloumn Adhaar_card;
+desc student;
+select studentname,section from student order by Percentage;
+select avg(Percentage) from student;
+select count(*) from student;
+select min(Percentage) from student;
+select max(Percentage) from student;
+select sum(Percentage)/count(*) from student where Substream = "Computer Science"
+delete from student where classstream = "Science";
+select * from student;
+
+
+#18
+#program to Integrate SQL with Python by importing the MYSQL module record of employee and display the record.
+
+import mysql.connector as pysql
+conn=pysql.connect(host="localhost", user = "root", passwd = "q2w3e4r5t6y7", database = "empdb")
+cur=conn.cursor()
+cur.execute("select * from employee")
+for x in cur:
+	print(x)
+	
+	
+#19
+import mysql.connector as pysql
+conn=pysql.connect(host = "localhost", user = "root", passwd = "q2w3e4r5t6y7", database = "empdb")
+cur=conn.cursor()
+ans='y'
+while ans.lower()=='y':
+	empno=int(input("enter the emp_no to search:"))
+	query='select * from employee where emp_no=()'.format(empno)
+	cur.execute(query)
+	result=cur.fetchall()
+	if cur.rowcount==0:
+		print("sorry not found")
+	else:
+		print("%10s"%"Emp_no","%20s"%"emp_name","%15s"%"dept","%10s"%"hire_data","%10"%"hire_data","%10s"%"salary")
+		for row in result:
+			print("%10s"%row[0],"%20s"%row[1],"%15s"%row[3],"%10s"%row[4])
+	ans=input("seaarch one more? [y/n]:")
+	
+	
+#20
+import mysql.connector as pysql
+conn=pysql.connect(host = "localhost", user = "root", passwd = "q2w3e4r5t6y7", database = "studentdb")
+cur=conn.cursor()
+
+roll=int(input("Enter roll number:"))
+perc=int(input("Enter new percentage:"))
+
+print("Before updating record:")
+query1 = 'select * from student where roll=()'.format(roll)
+cur.execute(query1)
+record = cur.fetchone()
+print(record)
+
+query2='update student set Percentage = () where roll=()'.format(perc,roll)
+cur.execute(query2)
+conn.commit()
+record = cur.fetchone()
+
+print("After updating record:")
+query3 = 'select * from student where roll=()'.format(roll)
+cur.execute(query3)
+record = cur.fetchone()
+print(record)
